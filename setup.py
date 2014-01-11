d1c5133f85de7b44963de6af08a7825c147ceded
Ashwin Patti <ashwin.patti@gmail.com>
2014-01-11 00:45:27 -0800

    First checkin

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='Dukes11',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='Dukes 11 team app',
+      author='Ashwin Patti',
+      author_email='ashwin.pattie@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=['Flask>=0.7.2'],
      )
