c52014af85ffaf1a5587d069a188415aaa5b6615
Ashwin Patti <ashwin.patti@gmail.com>
2014-01-11 01:10:35 -0800

    Corrected application

--- wsgi/service.py
+++ wsgi/service.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__,static_url_path='')
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
