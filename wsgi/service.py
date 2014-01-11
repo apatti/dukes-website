377dacce5257da71a7879d3f3d24c227bb69ad3e
Ashwin Patti <ashwin.patti@gmail.com>
2014-01-11 01:04:11 -0800

    Corrected application

--- wsgi/service.py
+++ wsgi/service.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='')
 
 @app.route('/')
 def index():
-    return "Hello Dukes11 from OpenShift"
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run()
