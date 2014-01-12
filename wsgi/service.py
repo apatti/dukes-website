a7a070cba054f9497272fd15658adf8159ae753e
Ashwin Patti <ashwin.patti@gmail.com>
2014-01-11 23:54:33 -0800

    Player.html

--- wsgi/service.py
+++ wsgi/service.py
@@ -7,5 +7,9 @@ app = Flask(__name__,static_url_path='')
 def index():
     return render_template('index.html')
 
+@app.route('/player.html')
+def playerIndex():
+    return render_template('player.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
