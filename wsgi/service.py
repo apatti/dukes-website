b4ae7d11da6f73cdfaeacba93537cdbedf6cd3f2
Ashwin Patti <ashwin.patti@gmail.com>
2014-01-13 21:37:18 -0800

    503 error

--- wsgi/service.py
+++ wsgi/service.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__,static_url_path='')
 
-@app.route('/api/')
+@app.route('/')
 def index():
-    return 'Hello World'
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
