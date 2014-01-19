a7754165ba5646d10114a11e5e66b087406d45f8
Ashwin Patti <ashwin.patti@gmail.com>
2014-01-19 01:45:21 -0800

    bug fix

--- wsgi/service.py
+++ wsgi/service.py
@@ -16,9 +16,9 @@ def insertUser():
     print("Good")
     userObj = request.get_json(force=True)
     print(userObj)
-    connection.request('POST','/1/classes/user',json.dumps(userObj),{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
-    result = json.loads(connection.getresponse().read())
-    return jsonify({'result':result}),200
+    #connection.request('POST','/1/classes/user',json.dumps(userObj),{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
+    #result = json.loads(connection.getresponse().read())
+    return jsonify({'result':'result'}),200
 
 @app.errorhandler(400)
 def invalid_data_format(error):
