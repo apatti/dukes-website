ad605edf3689f311aed9b96bfb34991c56b40ec5
Ram <rammohan.babu@gmail.com>
2014-01-19 00:18:33 -0800

    updaed

--- wsgi/static/dukesjs/login.js
+++ wsgi/static/dukesjs/login.js
@@ -26,6 +26,7 @@ window.fbAsyncInit = function() {
       // result from direct interaction from people using the app (such as a mouse click)
       // (2) it is a bad experience to be continually prompted to login upon page load.
 	  $('ul#mb1 li#profileTab').css('display','none');
+	  localStorage.setItem('USER_LOGGEDIN',false);
       FB.login();
     } else {
       // In this case, the person is not logged into Facebook, so we call the login() 
@@ -33,6 +34,7 @@ window.fbAsyncInit = function() {
       // of whether they are logged into the app. If they aren't then they'll see the Login
       // dialog right after they log in to Facebook. 
       // The same caveats as above apply to the FB.login() call here.
+	   localStorage.setItem('USER_LOGGEDIN',false);
 	  $('ul#mb1 li#profileTab').css('display','none');
       FB.login();
     }
@@ -55,7 +57,9 @@ window.fbAsyncInit = function() {
     FB.api('/me', function(response) {
       console.log('Good to see you, ' + response.name + '.');
 	  
-	  localStorage.setItem('fb_userInfo', response);
+	  localStorage.setItem('USER_LOGGEDIN',true);
+	  localStorage.setItem('USER_FB_INFO',JSON.stringify(response));
+	  
 	$('ul#mb1 li#profileTab').css('display','true');
     });
   }
