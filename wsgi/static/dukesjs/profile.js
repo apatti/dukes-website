63fd4126d848f6e274be5c4c7b9890ab2ee78b55
Ram <rammohan.babu@gmail.com>
2014-01-19 02:40:57 -0800

    adding profile.html profile.js

--- wsgi/static/dukesjs/profile.js
+++ wsgi/static/dukesjs/profile.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
  $(function() {
         $('#default').puipanel();
+		var name = localStorage.getItem('name');
+		$('#userPrfile').html("<h3> UserName : "+name+"</h3>");
 	});
 });
 
