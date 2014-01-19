cfb52d479f2367feb4aa2d38b0d38b8aee504f63
Ram <rammohan.babu@gmail.com>
2014-01-19 04:42:24 -0800

    updated

--- wsgi/static/dukesjs/profile.js
+++ wsgi/static/dukesjs/profile.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
  $(function() {
         $('#default').puipanel();
-		$('#basic').puidropdown();
+		$('#basic').puidropdown({
+			icon: 'ui-icon-check'
+		});
+		$('#associateBtn').puibutton();
 		var userData = $.parseJSON(localStorage.getItem('USER_FB_INFO'));
 		$('#nameDiv').html("<h3>Name : </h>"+userData['name']);
 		$('#locationDiv').html("<h3>Location : </h>"+userData['location']['name']);
