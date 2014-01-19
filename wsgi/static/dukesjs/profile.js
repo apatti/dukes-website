3b7d91bef0b268c6dc84e1234ed9d8dc00ef14aa
Ram <rammohan.babu@gmail.com>
2014-01-19 04:16:54 -0800

    bug fix

--- wsgi/static/dukesjs/profile.js
+++ wsgi/static/dukesjs/profile.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 		var userData = $.parseJSON(localStorage.getItem('USER_FB_INFO'));
 		$('#nameDiv').html("<h3>Name : </h>"+userData['name']);
 		$('#locationDiv').html("<h3>Location : </h>"+userData['location']['name']);
-		$('fbProfileImg').html("<img src='https://graph.facebook.com/"+userData['username']+"/picture?type=normal'  class='image' width='100px' height='100px'/>");
+		$('#fbProfileImg').html("<img src='https://graph.facebook.com/"+userData['username']+"/picture?type=normal'  class='image' width='100px' height='100px'/>");
 		
 	});
 });
