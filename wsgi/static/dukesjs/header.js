fdf1ecb60d53e623d0dd9ca4125ba9a39054942a
Ram <rammohan.babu@gmail.com>
2014-01-18 19:02:01 -0800

    added home.js

--- wsgi/static/dukesjs/header.js
+++ wsgi/static/dukesjs/header.js
@@ -1,8 +1,4 @@
  $(document).ready(function(){
-                $('#mb1').puimenubar();
-                
-                $('#mb2').puimenubar({
-                    autoDisplay: false
-                });
+    $('#dukesLoginDiv').html("<div id='fb-root'><fb:login-button show-faces='true' width='200' max-rows='1'></fb:login-button></div>");            
 
  });
