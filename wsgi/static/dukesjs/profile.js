cdfc1c78eb1d799a3759c116d324106c86e6187a
Ram <rammohan.babu@gmail.com>
2014-01-19 03:45:11 -0800

    bug fix

--- wsgi/static/dukesjs/profile.js
+++ wsgi/static/dukesjs/profile.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
  $(function() {
         $('#default').puipanel();
-		var userData = localStorage.getItem('USER_FB_INFO');
+		var userData = $.parseJSON(localStorage.getItem('USER_FB_INFO'));
 		$('#userPrfile').html("<h3> UserName : "+userData['name']+"</h3>");
 	});
 });
