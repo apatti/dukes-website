168d536393a0173a757121e950b1d9729e66f646
Ram <rammohan.babu@gmail.com>
2014-01-20 12:34:25 -0800

    updated profile tab on header

--- wsgi/static/dukesjs/profile.js
+++ wsgi/static/dukesjs/profile.js
@@ -16,11 +16,11 @@ $(document).ready(function() {
 			$('#nameDiv').html("<h3>Name : </h>"+userData['name']);
 			$('#locationDiv').html("<h3>Location : </h>"+userData['location']['name']);
 			$('#fbProfileImg').html("<img src='https://graph.facebook.com/"+userData['username']+"/picture?type=normal'  class='image' width='100px' height='100px'/>");
-			$('ul#mb1 li#profileTab').css('display','true');
+			
 		  } else if (response.status === 'not_authorized') {
 			// the user is logged in to Facebook, 
 			// but has not authenticated your app
-			$('ul#mb1 li#profileTab').css('display','none');
+			$('ul#mb1 li#profileTab').remove();
 			localStorage.removeItem('USER_FB_INFO');
 		  } else {
 			// the user isn't logged in to Facebook.
