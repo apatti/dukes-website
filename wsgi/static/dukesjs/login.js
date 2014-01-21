349437546773fc8ba5c8398447e3bd001ac5e643
Ram <rammohan.babu@gmail.com>
2014-01-20 18:04:47 -0800

    updated

--- wsgi/static/dukesjs/login.js
+++ wsgi/static/dukesjs/login.js
@@ -63,10 +63,11 @@ window.fbAsyncInit = function() {
 	   fbUserName = response.username;
 	   //Post FB data to dukes service
 	   var jsonObj ='';
+	   var dataTobesent ={ 'name':response.name,'first_name': response.first_name,'last_name':response.last_name, 'username':response.username,'fb_id':response.id,'link':response.link,'tca_id':1234,'email':''}
 	   $.ajax({
 			  type: "POST",
 			  url: DOMAIN_NAME +'/users',
-			  data: { name:response.name,first_name: response.first_name,last_name:response.last_name, username:response.username,fb_id:response.id,link:response.link,tca_id:1234,email:''},
+			  data:dataTobesent ,
 			   dataType: 'json',
 			  success: function(data,status,xht){
 				jsonObj = jQuery.parseJSON(JSON.stringify(data));
