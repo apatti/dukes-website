2ecc89a8efa0dbf8bd190d591b3e57ef089b2150
Ashwin Patti <ashwin.patti@gmail.com>
2014-01-21 03:19:05 -0800

    insert on unique in save

--- wsgi/service.py
+++ wsgi/service.py
@@ -25,11 +25,10 @@ def insertUserApi():
     userObj["first_name"]=reqObj.get("first_name")
     userObj["last_name"] = reqObj.get("last_name")
 
-    #print(json.dumps(userObj))
-    result = dukesuser.saveUser(userObj)
-    #connection.connect()
-    #connection.request('POST','/1/classes/user',json.dumps(userObj),{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
-    #result = json.loads(connection.getresponse().read())
+    result = getUser(userObj["username"])
+    if not result.get("results"):
+        result = dukesuser.saveUser(userObj)
+        
     return jsonify({'result':result}),201
 
 @app.route('/users/<username>',methods=['GET'])
