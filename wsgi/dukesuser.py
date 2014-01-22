765156abd57f35eb7a2c524816bd6dbdc792698b
Ashwin Patti <ashwin.patti@gmail.com>
2014-01-22 01:28:25 -0800

    mail

--- wsgi/dukesuser.py
+++ wsgi/dukesuser.py
@@ -22,7 +22,7 @@ def updateUser(userName,userObj,associate):
     if not existingUser.get("results"):
         abort(404)
     userid = existingUser.get("results")[0].get("objectId")
-    userObj.set("associated","0")
+    userObj.append("associated":0)
     connection.connect()
     connection.request('PUT','/1/classes/user/%s' % userid,json.dumps(userObj),{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
     result = json.loads(connection.getresponse().read())
