4cef842a0510c83f2453d25524d956472ef8c690
Ram <rammohan.babu@gmail.com>
2014-01-22 21:00:12 -0800

    added logout

--- wsgi/static/dukesjs/header.js
+++ wsgi/static/dukesjs/header.js
@@ -1,4 +1,4 @@
  $(document).ready(function(){
-    $('#dukesLoginDiv').html("<div id='fb-root'><fb:login-button show-faces='true' width='200' max-rows='1'></fb:login-button></div>");            
+    $('#dukesLoginDiv').html("<div id='fb-root'><fb:login-button autologoutlink='true' width='200' max-rows='1'></fb:login-button></div>");            
 
  });
