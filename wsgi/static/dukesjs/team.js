cec55e4017f5949e188c183e95c69386f1fd9efc
Ram <rammohan.babu@gmail.com>
2014-01-24 01:55:44 -0800

    rolled back

--- wsgi/static/dukesjs/team.js
+++ wsgi/static/dukesjs/team.js
@@ -3,30 +3,22 @@ $(document).ready(function(){
 	//Adding Player Image
 	  $.get("http://www.tennisballcricket.org/cricket_module/mobile_service.php?action=getTeamPlayers&tid=184",function(data,status){
          var pData = $.parseJSON(data);
-         
-          $.each(pData, function() {
-			createImage(this['pid'],this['fname'],this['lname']);	
-				
-             
+         var dataInTable ='';
+          $.each(pData, function() {			 
+              dataInTable = dataInTable + "<div class='player_img'>" + "<table><tr><td><a href='player.html?pid="+this['pid']+"' >"+getImageURL(fbUserName)+"</a></td></tr>"
+              dataInTable = dataInTable + "<tr><td>"+ this['fname'] +" "+ this['lname'] +"</td><tr>";
+              dataInTable = dataInTable + "</table></div>";
           });	
 	  //alert(dataInTable);
-                  
+         $("#teamDiv").html(dataInTable);         
     });  
 	
 });
 
-function createImage(pid,fName,lName){
-
-	$.get("http://www.dukesxi.co/users/tca/"+pid,function(momData,status){
-				var results = JSON.stringify(momData.user.results[0]);		
-				var userData = $.parseJSON(results);
-				var imgURL ='';
-				var dataInTable ='';
-				
-				 dataInTable = dataInTable + "<div class='player_img'>" + "<table><tr><td><a href='player.html?pid="+ pid +"' ><img src='https://graph.facebook.com/"+userData['username']+"/picture?type=normal'  class='image' width='100px' height='100px'/></a></td></tr>"
-              dataInTable = dataInTable + "<tr><td>"+ fName +" "+ lName +"</td><tr>";
-              dataInTable = dataInTable + "</table></div>";
-				$("#teamDiv").html(dataInTable);
-			});
-
+function getImageURL(username){
+var imgURL ='';
+  if(username.toLowerCase().indexOf("gottiganti")){
+		imgURL =  "<img src='https://graph.facebook.com/"+username+"/picture?type=normal'  class='image' width='100px' height='100px'/>"
+	  }
+	  return imgURL;
 }
