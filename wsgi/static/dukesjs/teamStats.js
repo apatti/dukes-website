ff1eca2331b8faf341c90c0bb6ac9e3319d9c3be
Ram <rammohan.babu@gmail.com>
2014-01-24 00:49:16 -0800

    updated

--- wsgi/static/dukesjs/teamStats.js
+++ wsgi/static/dukesjs/teamStats.js
@@ -7,8 +7,8 @@ $.get("http://tennisballcricket.org/cricket_module/mobile_service.php?action=get
             columns: [
                 {field:'matches_played', headerText: 'Mat', sortable:false},
                 {field:'wMatches', headerText: 'Wins', sortable:false},
-                {field:'lMatches', headerText: 'Lost', sortable:true},
-				{field:'tMatches', headerText: 'Match Draw', sortable:true}				
+                {field:'lMatches', headerText: 'Lost', sortable:false},
+				{field:'tMatches', headerText: 'Match Draw', sortable:false}				
                ],
 			   datasource: function(callback, ui) {  
 					
