c983b28c1e05a2c483ce8c120c74a543ace11e0a
Ram <rammohan.babu@gmail.com>
2014-01-24 00:39:01 -0800

    updated

--- wsgi/static/dukesjs/teamStats.js
+++ wsgi/static/dukesjs/teamStats.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
-
-
+$.get("http://tennisballcricket.org/cricket_module/mobile_service.php?action=getTeamStatDetails&tid=184",function(data,status){
+	
+	$('#teamStatsDiv').puidatatable({
+			lazy: true,
+            caption: 'Batting Stats',           
+            columns: [
+                {field:'matches_played', headerText: 'Mat', sortable:false},
+                {field:'wMatches', headerText: 'Wins', sortable:false},
+                {field:'runs_scored', headerText: 'Lost', sortable:true},
+				{field:'lMatches', headerText: 'Match Draw', sortable:true}				
+               ],
+			   datasource: function(callback, ui) {  
+					
+					var pData = $.parseJSON(data);							
+					callback.call(this, $.makeArray(pData));
+				}         
+        });
+	});
 });
 
 
