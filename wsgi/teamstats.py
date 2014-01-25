0c2a84b7aa1afcbc5e3d9576c6d453163fa632a9
Ashwin Patti <ashwin.patti@gmail.com>
2014-01-24 16:39:39 -0800

    login

--- wsgi/teamstats.py
+++ wsgi/teamstats.py
@@ -3,8 +3,11 @@ import json,requests
 def getTeamWL():
     statsResponse = requests.get('http://tennisballcricket.org/cricket_module/mobile_service.php?action=getTeamStatDetails&tid=184')
     stats = json.loads(statsResponse.content)
-    games = calculateWLRatio(stats.get("Games"))
+    (games,captainwins,captainties,captainlosses) = calculateWLRatio(stats.get("Games"))
     stats["Games"]=games
+    stats["CaptainWins"]=captainwins
+    stats["CaptainTies"]=captainties
+    stats["CaptainLosses"]=captainlosses
     return stats
 
 def calculateWLRatio(games):
@@ -12,17 +15,30 @@ def calculateWLRatio(games):
     l = 0.0
     t = 0.0
     gamesRatio = []
+    captains=[]
+    captainwins = {}
+    captainties ={}
+    captainlosses={}
     for game in games:
         if game['winner']=="3" :
+            captainties[game['captain']]=captainties.get(game['captain'],0)+1
             continue
         t = t+1
         if (game['bat_team1_id']=="184" and game['winner']=="1") or (game['bat_team2_id']=="184" and game['winner']=="2"):
             w=w+1
+            captainwins[game['captain']]=captainwins.get(game['captain'],0)+1
         if (game['bat_team1_id']=="184" and game['winner']=="2") or (game['bat_team2_id']=="184" and game['winner']=="1"):
             l=l+1
+            captainlosses[game['captain']]=captainlosses.get(game['captain'],0)+1
         r = (w)/(w+l)
         game["ratio"]=r
+        game["win"]=w
+        game["loss"]=l
+        game["tie"]=t
         game["match_date"]=game["match_date"].split(' ')[0]
         gamesRatio.append(game)
+    
         
-    return gamesRatio
+    print captainwins,captainties,captainlosses
+    #gamesRatio['Captains']= captains
+    return gamesRatio,captainwins,captainties,captainlosses
