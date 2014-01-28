e3e1822fa5c2665c9e9d04de3a65383b4f9cdaca
Ashwin Patti <ashwin.patti@gmail.com>
2014-01-28 00:52:41 -0800

    create poll

---
+++ wsgi/polls.py
@@ -0,0 +1,18 @@
+import json,httplib,urllib
+import urllib2
+import requests
+
+connection = httplib.HTTPSConnection('api.parse.com',443)
+
+def createPoll(pollObj):
+    connection.connect()
+    connection.request('POST','/1/classes/polls',json.dumps(pollObj),{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
+    result = json.loads(connection.getresponse().read())
+    return result
+
+def getPoll(pollId):
+    return
+
+def getPolls():
+    return
+
