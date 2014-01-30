d55d3cc5fd9f464e7e9c7e43bfd7f22bd0dd1a6d
Ram <rammohan.babu@gmail.com>
2014-01-29 20:33:03 -0800

    updated

--- wsgi/static/dukesjs/gallery.js
+++ wsgi/static/dukesjs/gallery.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 		galleryDiv = galleryDiv + '<ul>';		
 		$.each(pData, function() {			 
 			
-			galleryDiv = galleryDiv + '<li><img src="images/gallery/'+this['img']+'" alt="" title="" width="600px" height="450px"/></li>';
+			galleryDiv = galleryDiv + '<li><img src="images/gallery/'+this['img']+'" alt="" title="" width="800px" height="500px"/></li>';
 					
 		});
 		galleryDiv = galleryDiv + '</ul>';	
