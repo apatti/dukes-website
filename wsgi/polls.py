d2a629a436194b3ef2d8bdd93be8b9511899ba5f
Ashwin Patti <ashwin.patti@gmail.com>
2014-01-31 00:46:13 -0800

    take poll

--- wsgi/polls.py
+++ wsgi/polls.py
@@ -5,13 +5,15 @@ import requests
 connection = httplib.HTTPSConnection('api.parse.com',443)
 
 def createPoll(pollObj,optObj):
+    print pollObj
+    print optObj
     connection.connect()
     connection.request('POST','/1/classes/polls',json.dumps(pollObj),{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
     result = json.loads(connection.getresponse().read())
     pollId = result.get("objectId")
     if not pollId:
         abort(400)
-    
+    print pollId
     #poll is created, populate the options table.
     for option in optObj:
         option["pollid"]=pollId
