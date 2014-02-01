f5861f9e8ae8ead61be145850154eba4c6febc88
Ashwin Patti <ashwin.patti@gmail.com>
2014-02-01 02:25:28 -0800

    take poll

--- wsgi/polls.py
+++ wsgi/polls.py
@@ -39,14 +39,14 @@ def takePoll(poll_id,username,optid):
                     "id":optid
                     })})
 
-    #connection.request('GET','/1/classes/polloptions?%s'%params,'',{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
-    #result = json.loads(connection.getresponse().read())
+    connection.request('GET','/1/classes/polloptions?%s'%params,'',{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
+    result = json.loads(connection.getresponse().read())
     
     #now get the option id.
     #print result
-    #optionid=result.get("results")[0].get("objectId")
+    optionid=result.get("results")[0].get("objectId")
     #print optionid
-    connection.request('PUT','/1/classes/polloptions/%s'%params,json.dumps({"users":{"__op":"AddUnique","objects":[username]}}),{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"}) 
+    connection.request('PUT','/1/classes/polloptions/%s'%optionid,json.dumps({"users":{"__op":"AddUnique","objects":[username]}}),{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"}) 
     result = json.loads(connection.getresponse().read())
     
     return result
