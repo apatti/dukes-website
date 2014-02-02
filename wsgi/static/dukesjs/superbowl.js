5238f1c23be953f1d69f0fb5154512ffc19a2e30
Ashwin Patti <ashwin.patti@gmail.com>
2014-02-02 13:47:23 -0800

    superbowl

--- wsgi/static/dukesjs/superbowl.js
+++ wsgi/static/dukesjs/superbowl.js
@@ -1,3 +1,57 @@
+var DOMAIN_NAME = 'http://www.dukesxi.co';
+var username="none";
+
+ window.fbAsyncInit = function() {
+	  FB.init({
+		appId      : '627120887325860',
+		status     : true, // check login status
+		cookie     : true, // enable cookies to allow the server to access the session
+		xfbml      : true  // parse XFBML
+	  });
+	  FB.Event.subscribe('auth.logout', function(response) {
+	   logout();
+	   $('#loggedUserDiv').html("");
+		});
+		FB.Event.subscribe('auth.login', function() {
+		  location.reload();
+		});
+		
+		 FB.getLoginStatus(function(response) {
+		  if (response.status === 'connected') {
+			loggedIn();
+			var uid = response.authResponse.userID;			
+			var accessToken = response.authResponse.accessToken;
+			
+			
+		  } else if (response.status === 'not_authorized') {
+			// the user is logged in to Facebook, 
+			// but has not authenticated your app
+			localStorage.removeItem('USER_FB_INFO');
+		  } else {
+			// the user isn't logged in to Facebook.
+			localStorage.removeItem('USER_FB_INFO');
+		  }
+		 });
+		 applyCSSToPageComponents();
+ };
+	   
+function loggedIn(){
+ FB.api('/me', function(response) {
+	//var userData = $.parseJSON(localStorage.getItem('USER_FB_INFO'));
+	$('#loggedUserDiv').html(response.username);
+			$('#nameDiv').html("<h3>Name : </h>"+response.name);
+			$('#fNameDiv').html("<h3>First Name : </h>"+response.first_name);
+			$('#lNameDiv').html("<h3>Last Name : </h>"+response.last_name);
+			$('#fbLinkDiv').html("<h3>FB Link : </h>"+response.link);
+			Email : 
+			$('#fbLinkDiv').html("<h3>Email : </h><input id='emailTxt' type='text' />");
+			$('#fbProfileImg').html("<img src='https://graph.facebook.com/"+response.username+"/picture?type=normal' class='image' width='100px' height='100px'/>");
+			
+			username=response.username;
+ });
+}   
+
+
 $(document).ready(function(){
 	var superbowlObj ='';
 	$.get("/superbowl",function(data,status){
