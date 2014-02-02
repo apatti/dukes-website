3572cf456726225132324d7bb24302cf623aa966
Ashwin Patti <ashwin.patti@gmail.com>
2014-02-02 15:02:03 -0800

    superbowl

--- wsgi/superbowl.py
+++ wsgi/superbowl.py
@@ -4,6 +4,7 @@ import requests
 
 connection = httplib.HTTPSConnection('api.parse.com',443)
 def insertSuperBowl(superbowlObj):
+    print(superbowlObj)
     connection.connect()
     connection.request('POST','/1/classes/superbowl',json.dumps(superbowlObj),{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
     result = json.loads(connection.getresponse().read())
