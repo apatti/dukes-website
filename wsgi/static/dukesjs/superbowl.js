400ab3a61a8a4c849312865287bc005ae2920230
Ashwin Patti <ashwin.patti@gmail.com>
2014-02-02 16:41:14 -0800

    superbowl

--- wsgi/static/dukesjs/superbowl.js
+++ wsgi/static/dukesjs/superbowl.js
@@ -73,7 +73,7 @@ $(document).ready(function(){
 				for(i=0;i<data.results.length;i++)
 				    {
 					var pData = data.results[i];
-					callback.call($.makeArray(pData));
+					callback.call(pData);
 				    }
 				//var pData=data;
 				//callback.call(this,$.makeArray(pData));
