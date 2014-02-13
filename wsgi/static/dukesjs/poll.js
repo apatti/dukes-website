0ed433235b1076967f0bbc8ddc98af28e117b67c
Ram <rammohan.babu@gmail.com>
2014-02-13 00:18:22 -0800

    testing

--- wsgi/static/dukesjs/poll.js
+++ wsgi/static/dukesjs/poll.js
@@ -73,7 +73,7 @@ window.fbAsyncInit = function() {
 								$.each( uData,function () {
 									dropDownStr = dropDownStr + "<option value='"+userCount+"'>"+this+"</option>";
 									if(this.toString() === fbUserName){
-										hasPollTaken = 'true';
+										hasPollTaken = 'yes';
 									}
 									userCount ++;
 								});
