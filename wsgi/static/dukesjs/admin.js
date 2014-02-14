68f78516edf031e5156eb030fb075b02dbb02c44
Ram <rammohan.babu@gmail.com>
2014-02-13 20:38:58 -0800

    added admin.html

---
+++ wsgi/static/dukesjs/admin.js
@@ -0,0 +1,46 @@
+var fbUserName='';
+var DOMAIN_NAME = 'http://www.dukesxi.co';
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '627120887325860',
+    status     : true, // check login status
+    cookie     : true, // enable cookies to allow the server to access the session
+    xfbml      : true  // parse XFBML
+  });
+   FB.Event.subscribe('auth.logout', function(response) {
+	   logout();
+	   $('#pollCreationDiv').append('Please Login To See Admin Dashboard');
+	   location.reload();
+		});
+	FB.Event.subscribe('auth.login', function() {
+		  location.reload();
+	});
+	 FB.getLoginStatus(function(response) {
+		if (response.status === 'connected') {		
+			loggedIn();					
+		}else{
+			$('#pollCreationDiv').append('Please Login To See Admin Dashboard');
+		}
+	});
+	
+};
+ function loggedIn(){
+	 FB.api('/me', function(response) {
+		  console.log('Good to see you, ' + response.name + '.');	  
+		  localStorage.setItem('USER_FB_INFO',JSON.stringify(response));
+		
+		   fbUserName = response.username;
+		   $('#loggedUserDiv').html(response.username);	 
+		   if(fbUserName === 'pram.gottiganti' || fbUserName === 'ashwin.patti'){
+				pollCreation();
+		   }else{
+				$('#pollCreationDiv').append('You dont have permissions to view Admin Dashboard');
+		   }
+		});
+ }
+ /*
+	Poll Creation
+ */
+ function pollCreation(){
+ }
+ 
