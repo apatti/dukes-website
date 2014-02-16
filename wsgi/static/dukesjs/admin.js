5ef470e9ea1fcbd6a84c7cf194956e8b369a2574
Ram <rammohan.babu@gmail.com>
2014-02-16 00:14:22 -0800

    admin updated

--- wsgi/static/dukesjs/admin.js
+++ wsgi/static/dukesjs/admin.js
@@ -42,5 +42,23 @@ window.fbAsyncInit = function() {
 	Poll Creation
  */
  function pollCreation(){
+	$('#createNewPollBtn').click(function (){
+		
+		var question = $('#pollSubjectTxt').val();
+		var options ='';
+				
+		var options = [];
+		$('ul#pollOptionsUl').find('li').each(function(){						   
+			options.push('{ "text":"'+$(this).text()+'"}');
+		});
+		var submitJSON = '{';
+		submitJSON = submitJSON .append('username:'+fbUserName);
+		submitJSON = submitJSON .append('closeMethod:manual');
+		submitJSON = submitJSON .append('question:'+question);
+		submitJSON = submitJSON .append('endDate:2014-01-28');
+		submitJSON = submitJSON .append('options: ['+ options +']');
+		submitJSON = submitJSON .append('}');
+		alert(JSON.stringify(submitJSON);
+	});
  }
  
