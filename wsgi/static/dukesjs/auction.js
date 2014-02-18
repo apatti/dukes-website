37dbf7a3587fd1fcc012de51cc12a48cdcb3a684
Ram <rammohan.babu@gmail.com>
2014-02-17 19:31:49 -0800

    adding auction.html

---
+++ wsgi/static/dukesjs/auction.js
@@ -0,0 +1,44 @@
+var fbUserName='';
+var DOMAIN_NAME = 'http://www.dukesxi.co';
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '627120887325860',
+    status     : true, // check login status
+    cookie     : true, // enable cookies to allow the server to access the session
+    xfbml      : true  // parse XFBML
+  });
+   FB.Event.subscribe('auth.logout', function(response) {
+	   logout();
+	   $('#loginStatusDiv').show();
+	   location.reload();
+		});
+	FB.Event.subscribe('auth.login', function() {
+		  location.reload();
+	});
+	 FB.getLoginStatus(function(response) {
+		if (response.status === 'connected') {	
+			$('#loginStatusDiv').hide();		
+			auction();					
+		}else{
+			$('#loginStatusDiv').show();
+		}
+	});
+	
+};
+ function auction(){
+	 FB.api('/me', function(response) {
+		  console.log('Good to see you, ' + response.name + '.');	  
+		  localStorage.setItem('USER_FB_INFO',JSON.stringify(response));
+		
+		   fbUserName = response.username;
+		   $('#loggedUserDiv').html(response.username);	 
+		   polling();
+		});
+ }
+ 
+ 
+	function polling(){
+		
+		return;
+	}
+	
