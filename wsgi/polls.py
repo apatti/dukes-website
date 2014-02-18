183901037d34f6c9807573a15d20cad47dd517ce
Ashwin Patti <ashwin.patti@gmail.com>
2014-02-17 20:06:48 -0800

    delete poll

--- wsgi/polls.py
+++ wsgi/polls.py
@@ -41,8 +41,12 @@ tent-Type": "application/json"})
     
     params = urllib.urlencode({"where":json.dumps({"pollid":poll_id})})
     connection.connect()
-    connection.request('DELETE','/1/classes/polloptions?%s'%params,'',{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
-    connection.getresponse().read()
+    connection.request('GET','/1/classes/polloptions?%s'%params,'',{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
+    optionsObj=json.loads(connection.getresponse().read())
+    for optionObj in optionsObj:
+        connection.connect()
+        connection.request('DELETE','/1/classes/polloptions/%s'%optionObj.get("objectId"),'',{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
+        connection.getresponse().read()
     
     return result 
 
