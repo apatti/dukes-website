2761213f46afd4d5d2d5103f6d70d5aba4e89b9e
Ashwin Patti <ashwin.patti@gmail.com>
2014-03-13 23:36:29 -0700

    fantasy score update

--- wsgi/dukesfantasy.py
+++ wsgi/dukesfantasy.py
@@ -115,7 +115,6 @@ def calculateFantasyTeamScores(gameid):
     fantasyTeams = json.loads(connection.getresponse().read()).get("results")
     for fantasyTeam in fantasyTeams:
         points = 0
-        print fantasyTeam
         for player in players:
             if len([x for x in fantasyTeam.get("team") if re.match('%s' % player.get("player"), x)]) != 0:
                 points += player.get("points")
