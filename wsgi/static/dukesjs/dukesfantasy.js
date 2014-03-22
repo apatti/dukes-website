3a21d025c9d00af413dc44c5e54afd08c8c2f168
Ashwin Patti <ashwin.patti@gmail.com>
2014-03-22 03:13:04 -0700

    fantasy closed

--- wsgi/static/dukesjs/dukesfantasy.js
+++ wsgi/static/dukesjs/dukesfantasy.js
@@ -295,7 +295,7 @@ function allTeams(){
     });
 */
 
-    $.get("http://www.dukesxi.co/fantasyteam/MwcvuATX1T",function(data,status){
+    $.get("http://www.dukesxi.co/fantasyteam/jC7LXyXxDi",function(data,status){
     var rr = $.parseJSON(data);
     var dd = rr.results;
         var allTeamsTableStr ='<div class="pui-datatable-tablewrapper">';
