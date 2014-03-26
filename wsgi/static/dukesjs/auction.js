5d3c6ca132d98f59e35a1b41ff299f97f204ae8d
Ram <rammohan.babu@gmail.com>
2014-03-25 23:50:14 -0700

    testing

--- wsgi/static/dukesjs/auction.js
+++ wsgi/static/dukesjs/auction.js
@@ -59,12 +59,36 @@ window.fbAsyncInit = function() {
             var availableIPLPlayerstable = new google.visualization.Table(document.getElementById('iplPlayersDiv'));
 		    availableIPLPlayerstable.draw(datarow);
 			google.visualization.events.addListener(availableIPLPlayerstable, 'select', function() {
-				alert(availableIPLPlayerstable.getSelection());
-			  });
-        }
+				var selection = availableIPLPlayerstable.getSelection();
+				selection = visualization.getSelection();
+				var message = '';
+				  for (var i = 0; i < selection.length; i++) {
+					var item = selection[i];
+					if (item.row != null && item.column != null) {
+					  var str = data.getFormattedValue(item.row, item.column);
+					  message += '{row:' + item.row + ',column:' + item.column + '} = ' + str + '\n';
+					} else if (item.row != null) {
+					  var str = data.getFormattedValue(item.row, 0);
+					  message += '{row:' + item.row + ', (no column, showing first)} = ' + str + '\n';
+					} else if (item.column != null) {
+					  var str = data.getFormattedValue(0, item.column);
+					  message += '{(no row, showing first), column:' + item.column + '} = ' + str + '\n';
+					}
+				  }
+				  if (message == '') {
+					message = 'nothing';
+				  }
+				  alert('You selected ' + message);
+				});
+			}
 
     });
 }
+
+function selectHandler() {
+  
+}
+
 	function polling(){
         $.get(DOMAIN_NAME+"/ipl/users",function(data,status){
             var dd = data.results;
