3df4598e0a8d82e87f7f8da34c7e790a82eee35d
Ashwin Patti <ashwin.patti@gmail.com>
2014-03-29 00:57:26 -0700

    popup

--- wsgi/iplplayers.py
+++ wsgi/iplplayers.py
@@ -6,14 +6,14 @@ import requests
 connection = httplib.HTTPSConnection('api.parse.com',443)
 
 def getIplPlayers():
-    params = urllib.urlencode({"order":"Type"});
+    params = urllib.urlencode({"order":"Type","limit":300});
     connection.connect()
     connection.request('GET','/1/classes/iplplayers?%s' % params,'',{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
     #result = json.loads(connection.getresponse().read())
     return json.loads(connection.getresponse().read())
 
 def getIplAvailablePlayers():
-    params = urllib.urlencode({"where":json.dumps({"owner": ""}),"order":"Type"});
+    params = urllib.urlencode({"where":json.dumps({"owner": ""}),"order":"Type","limit":300});
     connection.connect()
     connection.request('GET','/1/classes/iplplayers?%s' % params,'',{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
     return json.loads(connection.getresponse().read())
