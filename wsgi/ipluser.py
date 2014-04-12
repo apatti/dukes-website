6ee13bcfc00c1e2a3c5b72666724bba14092bcc0
Ashwin Patti <ashwin.patti@gmail.com>
2014-04-12 03:06:03 -0700

    Next Bidder

--- wsgi/ipluser.py
+++ wsgi/ipluser.py
@@ -15,7 +15,7 @@ def saveIPLUser(userName,userTeam,userEmail):
     return updateUser(userName,userObj,0)
 
 def getIplUsers():
-    params = urllib.urlencode({"order" : "createdAt"})
+    params = urllib.urlencode({"order" : "-createdAt"})
     connection.connect()
     connection.request('GET','/1/classes/iplfantasy?%s' % params,'',{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
     #result = json.loads(connection.getresponse().read())
