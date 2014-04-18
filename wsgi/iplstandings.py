3b4181972d136c6dccf2263a55fe21ac9ce92871
Ashwin Patti <ashwin.patti@gmail.com>
2014-04-18 01:47:39 -0700

    score

--- wsgi/iplstandings.py
+++ wsgi/iplstandings.py
@@ -10,24 +10,30 @@ def getIplStanding():
     connection.request('GET','/1/classes/iplfantasyuserscore?','',{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
     result = json.loads(connection.getresponse().read())
     usersscores = result.get("results")
-    users = ['narashan','srudeep','rama.marri','balachandra.ambiga','vivek.vennam','gopi.k.mamidi','srikanth.kurmana','sagar.marri']
+
+    connection = httplib.HTTPSConnection('api.parse.com',443)
+    connection.connect()
+    connection.request('GET','/1/classes/iplfantasy?','',{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
+    result = json.loads(connection.getresponse().read())
+    users = result.get("results")
+
+    #users = ['narashan','srudeep','rama.marri','balachandra.ambiga','vivek.vennam','gopi.k.mamidi','srikanth.kurmana','sagar.marri']
     standings=[]
     for user in users:
         userscores = [userscore for userscore in usersscores if userscore["owner"]==user]
 
-        standing={}
-        standing["owner"]=user
-        standing["battingpoints"]=sum([userscore["battingpoints"] for userscore in userscores])
-        standing["bowlingpoints"]=sum([userscore["bowlingpoints"] for userscore in userscores])
-        standing["fieldingpoints"]=sum([userscore["fieldingpoints"] for userscore in userscores])
-        standing["mompoints"]=sum([userscore["mompoints"] for userscore in userscores])
-        standing["winpoints"]=sum([userscore["winpoints"] for userscore in userscores])
-        standing["win"]=sum([userscore["win"] for userscore in userscores])
-        standing["loss"]=sum([userscore["loss"] for userscore in userscores])
-        standing["tie"]=sum([userscore["tie"] for userscore in userscores])
-        standings.append(standing)
+        #standing={}
+        #standing["owner"]=user
+        user["battingpoints"]=sum([userscore["battingpoints"] for userscore in userscores])
+        user["bowlingpoints"]=sum([userscore["bowlingpoints"] for userscore in userscores])
+        user["fieldingpoints"]=sum([userscore["fieldingpoints"] for userscore in userscores])
+        user["mompoints"]=sum([userscore["mompoints"] for userscore in userscores])
+        user["winpoints"]=sum([userscore["winpoints"] for userscore in userscores])
+        user["win"]=sum([userscore["win"] for userscore in userscores])
+        user["loss"]=sum([userscore["loss"] for userscore in userscores])
+        user["tie"]=sum([userscore["tie"] for userscore in userscores])
+        standings.append(user)
 
-    print standings
     #sorted(standings,key=)
     return standings
 
