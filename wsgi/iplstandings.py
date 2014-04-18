684a8a57cff6d8af039c58923e51016acf0ab2c1
Ashwin Patti <ashwin.patti@gmail.com>
2014-04-17 19:05:44 -0700

    score

--- wsgi/iplstandings.py
+++ wsgi/iplstandings.py
@@ -3,11 +3,31 @@ import json,httplib,urllib
 import urllib2
 import requests
 
-connection = httplib.HTTPSConnection('api.parse.com',443)
 
 def getIplStanding():
+    connection = httplib.HTTPSConnection('api.parse.com',443)
     params = urllib.urlencode({"order":"wins,-loss,ties"});
     connection.connect()
     connection.request('GET','/1/classes/iplfantasy?%s' % params,'',{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
     #result = json.loads(connection.getresponse().read())
     return json.loads(connection.getresponse().read())
+
+def getIplCurrentWeekStanding():
+    #get current fantasyweek
+    connection = httplib.HTTPSConnection('api.parse.com',443)
+    connection.connect()
+    connection.request('GET','/1/classes/iplfantasycurrentweek/mJkDqrQ19R','',{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Con\
+tent-Type": "application/json"})
+    currentweeknumber = json.loads(connection.getresponse().read()).get("currentweeknumber")
+
+    #get the opponents.
+    params = urllib.urlencode({"week":currentweeknumber});
+    connection = httplib.HTTPSConnection('api.parse.com',443)
+    connection.connect()
+    connection.request('GET','/1/classes/iplfantasyplayerscore?%s' % params,'',{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
+
+    params = urllib.urlencode({"week":currentweeknumber})
+    connection = httplib.HTTPSConnection('api.parse.com',443)
+    connection.connect()
+    connection.request('GET','/1/classes/iplfantasyplayerscore?%s' % params,'',{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
+
