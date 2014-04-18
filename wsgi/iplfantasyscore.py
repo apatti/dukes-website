deefe73c6d7bd98cd19e14569d147db4497266b3
Ashwin Patti <ashwin.patti@gmail.com>
2014-04-17 20:54:53 -0700

    score

--- wsgi/iplfantasyscore.py
+++ wsgi/iplfantasyscore.py
@@ -11,7 +11,7 @@ def updateFantasyScore(iplTeamScoreObj):
     connection.request('GET','/1/classes/iplfantasycurrentweek/mJkDqrQ19R','',{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Con\
 tent-Type": "application/json"})
     currentweeknumber = json.loads(connection.getresponse().read()).get("currentweeknumber")
-
+    playerscores = []
     for fantasyScore in iplTeamScoreObj:
         battingpoints = 0
         bowlingpoints = 0
@@ -65,8 +65,54 @@ tent-Type": "application/json"})
         playerScoreObj["winpoints"]=winpoints
         playerScoreObj["week"]=currentweeknumber
         playerScoreObj["fieldingpoints"]=fieldingpoints
-
+        playerscores.extend(playerScoreObj)
         connection.request('POST','/1/classes/iplfantasyplayerscore',json.dumps(playerScoreObj),{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
+        result = json.loads(connection.getresponse().read())
+
+    # update user points
+    iplusers = ['narashan','srudeep','rama.marri','balachandra.ambiga','vivek.vennam','gopi.k.mamidi','srikanth.kurmana']
+    for ipluser in iplusers:
+        battingpoints=0
+        bowlingpoints=0
+        fieldingpoints=0
+        mompoints=0
+        winpoints=0
+        ipluserscores = [playerscore for playerscore in playerscores if playerscore["owner"]==ipluser]
+        for ipluserscore in ipluserscores:
+            battingpoints+= ipluserscore["battingpoints"]
+            bowlingpoints+= ipluserscore["bowlingpoints"]
+            fieldingpoints+= ipluserscore["fieldingpoints"]
+            mompoints+= ipluserscore["mompoints"]
+            winpoints+= ipluserscore["winpoints"]
+
+        userscore = {}
+        userscore["owner"]=ipluser
+        userscore["week"]=currentweeknumber
+        userscore["battingpoints"]=battingpoints
+        userscore["bowlingpoints"]=bowlingpoints
+        userscore["fieldingpoints"]=fieldingpoints
+        userscore["mompoints"]=mompoints
+        userscore["winpoints"]=winpoints
+
+        params = urllib.urlencode({"where": json.dumps({"owner": ipluser, "week": currentweeknumber})})
+        connection = httplib.HTTPSConnection('api.parse.com',443)
+        connection.connect()
+        connection.request('GET', '/1/classes/iplfantasyuserscore?%s' % params, '', {"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M", "X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
+        result = json.loads(connection.getresponse().read())
+        if len(result.get("results")) > 0:
+            operation = 'PUT'
+            userscore["battingpoints"]+=result.get("results")[0].get("battingpoints")
+            userscore["bowlingpoints"]+=result.get("results")[0].get("bowlingpoints")
+            userscore["fieldingpoints"]+=result.get("results")[0].get("fieldingpoints")
+            userscore["mompoints"]+=result.get("results")[0].get("mompoints")
+            userscore["winpoints"]+=result.get("results")[0].get("winpoints")
+            url = '/1/classes/iplfantasyuserscore/%s' % result.get("results")[0].get("objectId")
+        else:
+            operation = 'POST'
+            url = '/1/classes/iplfantasyuserscore'
+
+        connection.connect()
+        connection.request(operation, url, json.dumps(userscore), {"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
         result = json.loads(connection.getresponse().read())
 
     return result
