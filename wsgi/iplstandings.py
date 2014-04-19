9d2db35c33264449363843e2bbe153324053c3f6
Ashwin Patti <ashwin.patti@gmail.com>
2014-04-18 23:05:05 -0700

    score

--- wsgi/iplstandings.py
+++ wsgi/iplstandings.py
@@ -59,7 +59,7 @@ tent-Type": "application/json"})
     connection.request('GET', '/1/classes/dukesiplfantasyschedule?%s' % params, '', {"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M", "X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
     games = (json.loads(connection.getresponse().read())).get("results")
 
-    params = urllib.urlencode({"week":currentweeknumber})
+    params = urllib.urlencode({"where": json.dumps({"week": currentweeknumber})})
     connection = httplib.HTTPSConnection('api.parse.com',443)
     connection.connect()
     connection.request('GET','/1/classes/iplfantasyuserscore?%s' % params,'',{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
