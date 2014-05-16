cc4cfee85892239bde135008b3616d15536d5b26
Ashwin Patti <ashwin.patti@gmail.com>
2014-05-15 23:56:26 -0700

    rule

--- wsgi/playingteam.py
+++ wsgi/playingteam.py
@@ -39,7 +39,7 @@ def getPlayingTeam():
         })
     connection = httplib.HTTPSConnection('api.parse.com',443)
     connection.connect()
-    connection.request('GET', '/1/classes/playingteam/zR1SI0BrKv', '', {"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
+    connection.request('GET', '/1/classes/playingteam?%s' % params, '', {"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
     result = json.loads(connection.getresponse().read())
     return result
 
