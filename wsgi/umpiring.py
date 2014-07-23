d7e926c1aa55a9af53d28a1d6493bef49539ab15
Ashwin Patti <ashwin.patti@gmail.com>
2014-07-23 12:00:08 -0700

    Navigation bar for the umpire page. Order the umpiring list by name.

--- wsgi/umpiring.py
+++ wsgi/umpiring.py
@@ -6,6 +6,7 @@ import requests
 def getUmpireList():
     connection = httplib.HTTPSConnection('api.parse.com',443)
     connection.connect()
-    connection.request('GET','/1/classes/umpiring','',{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
+    params = urllib.urlencode({"order":"user"})
+    connection.request('GET','/1/classes/umpiring?%s' % params,'',{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
     result = json.loads(connection.getresponse().read())
     return result
