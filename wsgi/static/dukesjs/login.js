7a9467bb339f55708f2f4141b87703ff8ddb1068
Ashwin Patti <ashwin.patti@gmail.com>
2014-09-26 16:08:47 -0700

    google signin

--- wsgi/static/dukesjs/login.js
+++ wsgi/static/dukesjs/login.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
     "data-callback='googleSigninCallback'"+
     "data-clientid='319656721002-ppajmfotulboinl39ic9vq19ql60m0nq.apps.googleusercontent.com'"+
     "data-cookiepolicy='single_host_origin'"+
-    "data-requestvisibleactions='http://schema.org/AddAction'"+
     "data-scope='email'"+
     "data-width='200'"+
     "data-height='short'>"+
