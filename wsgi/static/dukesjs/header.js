0453522e96445a2be16fff36528e8c9d339bd4ac
Ashwin Patti <ashwin.patti@gmail.com>
2014-09-26 01:00:15 -0700

    google signin

--- wsgi/static/dukesjs/header.js
+++ wsgi/static/dukesjs/header.js
@@ -5,9 +5,9 @@
     "data-clientid='319656721002-ppajmfotulboinl39ic9vq19ql60m0nq.apps.googleusercontent.com'"+
     "data-cookiepolicy='single_host_origin'"+
     "data-requestvisibleactions='http://schema.org/AddAction'"+
-    "data-scope='https://www.googleapis.com/auth/plus.login'>"+
+    "data-scope='https://www.googleapis.com/auth/plus.login'"+
     "data-width='standard'"+
-    "data-height='short'"+
+    "data-height='short'>"+
     "</span><fb:login-button autologoutlink='true' width='200' max-rows='1'></fb:login-button></div>");
 
  });
