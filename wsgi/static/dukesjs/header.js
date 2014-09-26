cadab55da832a0456a8fde6244770d6f980863dd
Ashwin Patti <ashwin.patti@gmail.com>
2014-09-26 00:47:25 -0700

    google signin

--- wsgi/static/dukesjs/header.js
+++ wsgi/static/dukesjs/header.js
@@ -1,12 +1,11 @@
  $(document).ready(function(){
-	 $('#dukesLoginDiv').html("<div id='googleSigninButton'>"+
+	 $('#dukesLoginDiv').html("<div id='signinButton'>"+
   "<span class='g-signin' " +
     "data-callback='signinCallback'"+
-    "data-clientid='CLIENT_ID'"+
+    "data-clientid='319656721002-ppajmfotulboinl39ic9vq19ql60m0nq.apps.googleusercontent.com'"+
     "data-cookiepolicy='single_host_origin'"+
     "data-requestvisibleactions='http://schema.org/AddAction'"+
     "data-scope='https://www.googleapis.com/auth/plus.login'>"+
-  "</span></div>"+
-  "<div id='fb-root'><fb:login-button autologoutlink='true' width='200' max-rows='1'></fb:login-button></div>");
+  "</span><fb:login-button autologoutlink='true' width='200' max-rows='1'></fb:login-button></div>");
 
  });
