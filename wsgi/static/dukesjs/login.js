9a7e09bfc26b5ef095aab0cd8d35b9ef5cd450f5
Ashwin Patti <ashwin.patti@gmail.com>
2014-09-26 17:50:23 -0700

    google signin

--- wsgi/static/dukesjs/login.js
+++ wsgi/static/dukesjs/login.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
                         {
                             primaryEmail=profile.emails[i].value;
                             $('#loggedUserDiv').html(profile.displayName+'&nbsp&nbsp<img src="'+profile.image.url+'">');
-                            $('#d').html('<a id="googleSignOut">signout</a>')
+                            $('#dukesLoginDiv').html('<a id="googleSignOut">signout</a>')
                             localStorage.setItem('USER_GOOGLE_INFO',JSON.stringify(profile));
                             break;
                         }
