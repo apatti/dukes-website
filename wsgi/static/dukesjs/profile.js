da882766fd7e05af506023cf66341e8ef32e14d0
Ashwin Patti <ashwin.patti@gmail.com>
2014-10-01 17:34:44 -0700

    google signin

--- wsgi/static/dukesjs/profile.js
+++ wsgi/static/dukesjs/profile.js
@@ -9,7 +9,7 @@ function loggedIn(){
 			Email :
 			$('#emailTxtDiv').html("<h3>Email : </h><input id='emailTxt' type='text' value="+userData.email+">");
 			$('#profileImg').html("<img src="+userData.imagelink+" class='image' width='100px' height='100px'/>");
-            $('#centerContent').val("");
+            location.reload();
             applyCSSToPageComponents();
 }   
 
