fc9dca4b4d009fe5b8ad3801bf626b6ddf770b3c
Ashwin Patti <ashwin.patti@gmail.com>
2015-04-03 01:25:44 -0700

    bids

--- wsgi/iplbids.py
+++ wsgi/iplbids.py
@@ -12,7 +12,8 @@ def viewAllBids(league):
             }
         },"league":int(league)
     }),"order":"-createdAt","limit":300});
-    print params
+    print league
+    print int(league)
     connection = httplib.HTTPSConnection('api.parse.com',443)
     connection.connect()
     connection.request('GET','/1/classes/bidhistory?%s' % params,'',{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
