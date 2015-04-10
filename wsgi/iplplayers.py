7fde95affb752c702ad5bf531e301cd8bfff766b
Ashwin Patti <ashwin.patti@gmail.com>
2015-04-10 15:54:48 -0700

    owned players

--- wsgi/iplplayers.py
+++ wsgi/iplplayers.py
@@ -88,7 +88,7 @@ def getIplUserAvailablePlayers(username,leagueid):
 
 
 def getIplTeamPlayers(team):
-    params = urllib.urlencode({"where":{"Team",team},"order":"Type","limit":300});
+    params = urllib.urlencode({"where":json.dumps({"Team":team,"$or":[{"owner1":{"$ne":""}},{"owner2":{"$ne":""}}]}),"order":"Type","limit":300});
     connection = httplib.HTTPSConnection('api.parse.com',443)
     connection.connect()
     connection.request('GET','/1/classes/iplplayer?%s' % params,'',{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
