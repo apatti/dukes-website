b3eb87ad97c3dba6ad46624185fc2ac61c2558b8
Ashwin Patti <ashwin.patti@gmail.com>
2015-04-12 16:19:33 -0700

    bid edit

--- wsgi/iplbids.py
+++ wsgi/iplbids.py
@@ -30,7 +30,7 @@ def viewUserBids(username,league):
 
 def getBidPrority(username,amount):
     params = urllib.urlencode({"where":json.dumps({"username": username,"bidamount":amount, "priority": {"$ne": -1}}),
-                               "order": "priority", "limit":1});
+                               "order": "-priority", "limit":1});
     connection = httplib.HTTPSConnection('api.parse.com',443)
     connection.connect()
     connection.request('GET','/1/classes/iplfantasybids?%s' % params,'',{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
