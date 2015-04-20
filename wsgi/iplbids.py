02b41d16ebd086294418dd46de20619c543c74f0
Ashwin Patti <ashwin.patti@gmail.com>
2015-04-19 23:44:00 -0700

    date edit

--- wsgi/iplbids.py
+++ wsgi/iplbids.py
@@ -9,7 +9,7 @@ def viewAllTransactions(league):
 
     connection = httplib.HTTPSConnection('api.parse.com',443)
     connection.connect()
-    connection.request('GET','/1/classes/bidhistory?%s' % params,'',{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
+    connection.request('GET','/1/classes/iplbidresult?%s' % params,'',{"X-Parse-Application-Id": "ioGYGcXuXi2DRyPYnTLB6lTC5DSPtiLbOhAU9P1M","X-Parse-REST-API-Key": "3yuAKMX4bz8QouVmfWBODyleTV5GzD3yhn2yYzYo","Content-Type": "application/json"})
     #result = json.loads(connection.getresponse().read())
     return json.loads(connection.getresponse().read())
 
