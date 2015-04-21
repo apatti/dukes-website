0dc0d5854391ad440c8e3152462692d7b8aa70e9
Ashwin Patti <ashwin.patti@gmail.com>
2015-04-20 21:54:11 -0700

    date edit

--- wsgi/iplbids.py
+++ wsgi/iplbids.py
@@ -124,7 +124,7 @@ def processFABids():
     rankings.reverse()
 
     #getbids order by amount.
-    params = urllib.urlencode({"where":json.dumps({"league":'1'}),
+    params = urllib.urlencode({"where":json.dumps({"league":'2'}),
                                "order": "-bidamount,priority"});
     connection = httplib.HTTPSConnection('api.parse.com',443)
     connection.connect()
