7354c223c65afa01236cca25b7f3a18314ba2d7f
Ashwin Patti <ashwin.patti@gmail.com>
2015-09-11 15:46:28 -0700

    viewport

--- wsgi/static/dukesjs/home.js
+++ wsgi/static/dukesjs/home.js
@@ -50,9 +50,9 @@ $(document).ready(function(){
             }
             var playerOfTheWeek = '';
             playerOfTheWeek = playerOfTheWeek + "<table cellspacing='0px'><tr>";
-            playerOfTheWeek = playerOfTheWeek + "<td rowspan='2'><img  src='"+bowlerImage+"'  class='image' width='75px' height='75px'/></td>";
-            playerOfTheWeek = playerOfTheWeek + "<td><h3><a href=/player.html?pid="+bowler.PlayerId+">"+ bowler.FirstName +" "+bowler.LastName+"</a></h3></td></tr>";
-            playerOfTheWeek = playerOfTheWeek + "<tr><td>("+bowler.Overs +"-"+ bowler.Maidens+"-"+bowler.Runs+"-"+bowler.Wickets+")</td></tr>";
+            playerOfTheWeek = playerOfTheWeek + "<td><img  src='"+bowlerImage+"'  class='image' width='75px' height='75px'/></td>";
+            playerOfTheWeek = playerOfTheWeek + "<td><h3><a href=/player.html?pid="+bowler.PlayerId+">"+ bowler.FirstName +" "+bowler.LastName+"</a></h3><br/>";
+            playerOfTheWeek = playerOfTheWeek + "("+bowler.Overs +"-"+ bowler.Maidens+"-"+bowler.Runs+"-"+bowler.Wickets+")</td></tr>";
             playerOfTheWeek = playerOfTheWeek + "</table>";
             $("#playerOfTheWeekDiv").append(playerOfTheWeek);
             /*
