5e6857a98e345cdf28d214ffc3ed0208a2ef1e20
Ashwin Patti <ashwin.patti@gmail.com>
2015-09-11 15:56:41 -0700

    viewport

--- wsgi/static/dukesjs/home.js
+++ wsgi/static/dukesjs/home.js
@@ -37,9 +37,9 @@ $(document).ready(function(){
             }
             var playerOfTheWeek = '';
             playerOfTheWeek = playerOfTheWeek + "<table cellspacing='0px'><tr>";
-            playerOfTheWeek = playerOfTheWeek + "<td rowspan='2'><img  src='"+batsmanImage+"'  class='image' width='75px' height='75px'/></td>";
-            playerOfTheWeek = playerOfTheWeek + "<td><a href=/player.html?pid="+batsman.PlayerId+">"+ batsman.FirstName +" "+batsman.LastName+"</a></td>";
-            playerOfTheWeek = playerOfTheWeek + "<tr><td>("+batsman.RunScored +" of "+ batsman.BallFaced+")</td></tr>";
+            playerOfTheWeek = playerOfTheWeek + "<td><img  src='"+batsmanImage+"'  class='image' width='75px' height='75px'/></td>";
+            playerOfTheWeek = playerOfTheWeek + "<td><a href=/player.html?pid="+batsman.PlayerId+">"+ batsman.FirstName +" "+batsman.LastName+"</a><br/>";
+            playerOfTheWeek = playerOfTheWeek + "("+batsman.RunScored +" of "+ batsman.BallFaced+")</td>";
             playerOfTheWeek = playerOfTheWeek + "</tr></table>";
             $("#playerOfTheWeekDiv").append(playerOfTheWeek);
             bowlerData = getPlayerRecord(bowler.PlayerId);
