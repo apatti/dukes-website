f0cb94c05014d4297c6338f915b792aa3c01a280
Ashwin Patti <ashwin.patti@gmail.com>
2016-03-27 00:57:43 -0700

    close poll changes

--- wsgi/polls.py
+++ wsgi/polls.py
@@ -112,11 +112,12 @@ def closePoll(poll_id):
     for users in pollCursor:
         pollUsers[users["text"]]=users["users"]
     
+    polldocument = db.polls.find_one({"_id":poll_id},["question"])
     #admins = getUserHelper("isAdmin",True)
-    sendPollCloseMail(pollUsers)
+    sendPollCloseMail(pollUsers,polldocument.question)
     pass 
 
-def sendPollCloseMail(pollUsers):
+def sendPollCloseMail(pollUsers,title):
     admins = getUserHelper("isAdmin",True)
     messageBody ='<div style="margin-bottom:3px;font-weight:bold">Available:'
     for user in pollUsers['Available']:
@@ -127,7 +128,7 @@ def sendPollCloseMail(pollUsers):
         messageBody += '<div style="margin-bottom:5px>'+user+'</div>'
     messageBody += '</div>'
 
-    message ='<html><body style="margin:0"><div style="text-align: center;background-color: #eee;width:100%;margin:0;padding:30px 0;font-family:Helvetica Neue Light, Lucida Grande, Helvetica, Arial;font-size: 12px;"><div style="width:96%;max-width:660px;margin:0 auto;"><table style="color:#000;background-color: #eee;margin: 10px auto; border: none;border-collapse: collapse;width: 100%;"><tbody><tr><td style="width:15%;text-align: left;"><img style="width: 80px;height: 80px;" src="http://www.dukesxi.co/images/Dukes+logo+red.jpg"></td><td style="width:85%;text-align: left;"><h1 style="font-size: 22px;padding-bottom: 5px;font-weight: bold;margin:0;">Poll Closure Mail</h1><div style="color: #689;">Message initiated by <span style="font-weight: bold;"> Dukes XI Web Service</div></td></tr></tbody></table><table style="color:#000;background-color:#fff;margin:10px auto;border:none;border-collapse:collapse;width:100%;"><tbody><tr><td style="text-align: left;font-size: 14px;padding: 30px;height: 400px;vertical-align: top;">'
+    message ='<html><body style="margin:0"><div style="text-align: center;background-color: #eee;width:100%;margin:0;padding:30px 0;font-family:Helvetica Neue Light, Lucida Grande, Helvetica, Arial;font-size: 12px;"><div style="width:96%;max-width:660px;margin:0 auto;"><table style="color:#000;background-color: #eee;margin: 10px auto; border: none;border-collapse: collapse;width: 100%;"><tbody><tr><td style="width:15%;text-align: left;"><img style="width: 80px;height: 80px;" src="http://www.dukesxi.co/images/Dukes+logo+red.jpg"></td><td style="width:85%;text-align: left;"><h1 style="font-size: 22px;padding-bottom: 5px;font-weight: bold;margin:0;">Poll'+title+' closed</h1><div style="color: #689;">Message initiated by <span style="font-weight: bold;"> Dukes XI Web Service</div></td></tr></tbody></table><table style="color:#000;background-color:#fff;margin:10px auto;border:none;border-collapse:collapse;width:100%;"><tbody><tr><td style="text-align: left;font-size: 14px;padding: 30px;height: 400px;vertical-align: top;">'
     message += messageBody;
     message +='</td></tr></tbody></table></div></div></body></html>'
-    mail.send_mail_to(message,"ashwin.patti@gmail.com","","Poll closure")
+    mail.send_mail_to(message,"ashwin.patti@gmail.com","","Poll"+title+" closed")
