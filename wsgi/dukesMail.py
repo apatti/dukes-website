2418f5522e0cdc905dce1203393448d6a2af1072
Ashwin Patti <ashwin.patti@gmail.com>
2016-03-27 17:16:49 -0700

    close poll changes

--- wsgi/dukesMail.py
+++ wsgi/dukesMail.py
@@ -10,6 +10,7 @@ def send_mail_to(message,to,cc,subject):
 
 def send_html_mail_to(message,to,cc,subject):
     print "Sending mail to %s" % to
+    print message
     requests.post("https://api.mailgun.net/v2/dukesxi.co/messages",auth=("api","key-6juj8th780z4bbbf1jpl7ffpx5z34wa9"),data={"from":"Dukes XI <cricketteam@dukesxi.co>","to":to,"cc":cc,"subject":subject,"html":message})
 
 def send_mail_all(message,cc,subject):
