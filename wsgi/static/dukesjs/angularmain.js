a55d8b1792bbb726f2757d6442afa6be71e8ee4f
Ashwin Patti <ashwin.patti@gmail.com>
2016-04-01 22:39:46 -0700

    mobile

--- wsgi/static/dukesjs/angularmain.js
+++ wsgi/static/dukesjs/angularmain.js
@@ -3,5 +3,21 @@
  */
 
 var app = angular.module('dukesCricketApp',['ui.bootstrap',
-    'ngRoute'
+					    'ngRoute','dukesControllers'
 ]);
+
+app.config(['$routeProvide',
+	    function($routeProvider){
+		   $routeProvider.
+		       when('/home',{
+			       templateUrl: 'partials/dukes-home.html',
+				   //controller: 'homeCtrl'
+				   });
+	       }]);
+
+var homeCtrl = angular.module('dukesControllers',[]);
+
+homeCtrl.controller('homeCtrl',['$scope','$http',
+				function($scope,$http){
+			    return;
+			}]);
