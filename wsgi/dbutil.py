46ce9664a84c1c7ca982f2648f5110e5ce9b9721
Ashwin Patti <ashwin.patti@gmail.com>
2017-09-29 20:23:35 -0700

    remove mongo hard coded

--- wsgi/dbutil.py
+++ wsgi/dbutil.py
@@ -4,8 +4,6 @@ import os
 
 if 'MONGODB_STRING' in os.environ:
     mongodb = os.environ['MONGODB_STRING']
-else:
-    mongodb = 'mongodb://dukesadmin:duke511@ds051665.mlab.com:51665/dukesxi'
 
 def getdbObject():
     client = MongoClient(mongodb)
